44edf23b96fd416cec3d834aa486d49e8e3580ff
Salim-54 <ndayisabyesalim54@gmail.com>
2023-03-08 05:36:30 +0200

    pushed some

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-const express = require('express');
-http = require('http');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+// import dotenv variables
+require("dotenv").config();
+http = require("http");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const dishRouter = require('./routes/dishRouter');
-const promoRouter = require('./routes/promoRouter');
-const leaderRouter = require('./routes/leaderRouter');
+const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
-app.use(express.static(__dirname + '/public'));
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
+app.use(express.static(__dirname + "/public"));
 
-const hostname = process.env.HOST;
-const port = process.env.PORT;
+// const hostname = "localhost";
+//import port from .env file
+const port = process.env.PORT || 7000;
+// import hostname from .env file
+const hostname = process.env.HOST || "localhost";
 
 app.use((req, res, next) => {
-console.log(req.headers);
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
 
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
